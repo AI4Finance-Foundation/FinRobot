ea72ae525a5efa011480d1d11ff5ff865204a5c8
Bruce Yang ByFinTech <hongyang.yang@columbia.edu>
2024-03-01 03:24:27 +0800

    2024-03-01

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+# Read requirements.txt, ignore comments
+try:
+    with open("requirements.txt", "r") as f:
+        REQUIRES = [line.split('#', 1)[0].strip() for line in f if line.strip()]
+except:
+    print("'requirements.txt' not found!")
+    REQUIRES = list()
+
+setup(
+    name="FinGPT",
+    version="0.0.1",
+    include_package_data=True,
+    author="AI4Finance Foundation",
+    author_email="contact@ai4finance-foundation.org",
+    url="https://github.com/AI4Finance-Foundation/FinRobot",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=REQUIRES,
+    description="FinRobot: An Open-Source AI Agent Platform for Financial Applications using LLMs",
+    long_description="""FinRobot""",
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    keywords="Financial Large Language Models, AI Agents",
+    platforms=["any"],
+    python_requires=">=3.6",
+)

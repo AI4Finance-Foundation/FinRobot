40fe58d10360bd1f0044f677e558dcd20739bb62
Noir97 <wangnengtututu@gmail.com>
2024-04-11 21:35:09 +0800

    update multimodal tutorial

---
+++ finrobot/functional/charting.py
@@ -0,0 +1,54 @@
+import mplfinance as mpf
+from typing import Annotated, List, Tuple
+
+from ..data_source.yfinance_utils import YFinanceUtils
+
+
+class MplFinanceUtils:
+
+    def plot_stock_price_chart(
+        ticker_symbol: Annotated[str, "Ticker symbol of the stock (e.g., 'AAPL' for Apple)"],
+        start_date: Annotated[str, "Start date of the historical data in 'YYYY-MM-DD' format"],
+        end_date: Annotated[str, "End date of the historical data in 'YYYY-MM-DD' format"],
+        save_path: Annotated[str, "File path where the plot should be saved"],
+        verbose: Annotated[str, "Whether to print stock data to console. Default to True."] = True,
+        type: Annotated[str, "Type of the plot, should be one of 'candle','ohlc','line','renko','pnf','hollow_and_filled'. Default to 'candle'"] = "candle",
+        style: Annotated[str, "Style of the plot, should be one of 'default','classic','charles','yahoo','nightclouds','sas','blueskies','mike'. Default to 'default'."] = 'default',
+        mav: Annotated[int | List[int] | Tuple[int, ...] | None, "Moving average window(s) to plot on the chart. Default to None."] = None,
+        show_nontrading: Annotated[bool, "Whether to show non-trading days on the chart. Default to False."] = False,
+    ) -> str:
+        """
+        Plot a candlestick chart using mplfinance for the specified stock and time period,
+        and save the plot to a file.
+        """
+        # Fetch historical data
+        stock_data = YFinanceUtils.get_stock_data(ticker_symbol, start_date, end_date)
+        if verbose:
+            print(stock_data.to_string())
+
+        params = {
+            'type': type,
+            'style': style,
+            'title': f'{ticker_symbol} {type} chart',
+            'ylabel': 'Price',
+            'volume': True,
+            'ylabel_lower': 'Volume',
+            'mav': mav,
+            'show_nontrading': show_nontrading,
+            'savefig': save_path
+        }
+        # Using dictionary comprehension to filter out None values (MplFinance does not accept None values)
+        filtered_params = {k: v for k, v in params.items() if v is not None}
+
+        # Plot chart
+        mpf.plot(stock_data, **filtered_params)
+
+        return f"{type} chart saved to <img {save_path}>"
+
+
+if __name__ == "__main__":
+    # Example usage:
+    start_date = '2024-03-01'
+    end_date = '2024-04-01'
+    save_path = 'AAPL_candlestick_chart.png'
+    MplFinanceUtils.plot_candlestick_chart('AAPL', start_date, end_date, save_path)

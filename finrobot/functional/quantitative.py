a8d50dabd7aac360ebe420a07c66241735c74ec6
Noir97 <wangnengtututu@gmail.com>
2024-04-19 19:29:19 +0800

    rewrite backtester

--- finrobot/functional/quantitative.py
+++ finrobot/functional/quantitative.py
@@ -1,10 +1,11 @@
 import os
 import json
 import pickle
-import backtrader as bt
+import importlib
 import yfinance as yf
+import backtrader as bt
+from backtrader.strategies import SMA_CrossOver
 from typing import Annotated, List, Tuple
-from functools import wraps
 
 
 class BackTraderUtils:
@@ -20,7 +21,8 @@ class BackTraderUtils:
             str, "End date of the historical data in 'YYYY-MM-DD' format"
         ],
         strategy: Annotated[
-            str, "BackTrader Strategy class to be backtested. module:class"
+            str,
+            "BackTrader Strategy class to be backtested. Can be pre-defined or custom. Pre-defined options: 'SMA_CrossOver'. If custom, provide module path and class name as a string like 'my_module:TestStrategy'.",
         ],
         params: Annotated[
             dict,
@@ -32,15 +34,17 @@ class BackTraderUtils:
         """
         cerebro = bt.Cerebro()
 
-        # Deserialize and Add a strategy
-        with open(os.path.join(load_dir, "strategy.pkl"), "rb") as f:
-            strategy_func = pickle.load(f)
+        if strategy == "SMA_CrossOver":
+            strategy_class = SMA_CrossOver
+        else:
+            assert (
+                ":" in strategy
+            ), "Custom strategy should be module path and class name separated by a colon."
+            module_path, class_name = strategy.split(":")
+            module = importlib.import_module(module_path)
+            strategy_class = getattr(module, class_name)
 
-        with open(os.path.join(load_dir, "config.json"), "r") as f:
-            loaded_params = json.load(f)
-            strategy_class = strategy_func(**loaded_params)
-
-        cerebro.addstrategy(strategy_class)
+        cerebro.addstrategy(strategy_class, **params)
 
         # Create a data feed
         data = bt.feeds.PandasData(
@@ -59,5 +63,13 @@ if __name__ == "__main__":
     start_date = "2011-01-01"
     end_date = "2012-12-31"
     ticker = "MSFT"
-    BackTraderUtils.create_sma_cross(10, 30, "sma_cross")
-    BackTraderUtils.back_test(ticker, start_date, end_date, "sma_cross")
+    # BackTraderUtils.back_test(
+    #     ticker, start_date, end_date, "SMA_CrossOver", {"fast": 10, "slow": 30}
+    # )
+    BackTraderUtils.back_test(
+        ticker,
+        start_date,
+        end_date,
+        "test_module:TestStrategy",
+        {"exitbars": 5},
+    )
